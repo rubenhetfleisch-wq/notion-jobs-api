33d4555688ae60a837e9f557fc8b41ad4574c028
rubenhetfleisch-wq <ruben.hetfleisch@aiaustria.com>
2026-01-02 18:59:59 +0100

    Update jobs.js

--- api/jobs.js
+++ api/jobs.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
         checkbox: { equals: true }
       },
       sorts: [
-        { property: "Date", direction: "ascending" }
+        { property: "Added", direction: "ascending" }
       ]
     });
 
